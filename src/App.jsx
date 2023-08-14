2d84c7d88ac7974e68e434eeac8ba1e3a4f73622
BASTIEN VITOUR <bastien.vitour@edu.esiee-it.fr>
2023-08-14 16:19:52 +0200

    ajouté les fonctions

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,50 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.scss';
 import Form from 'react-bootstrap/Form'
+
+import { rgbToHex, rgbToHsl, hexToRgb, hexToHsl, hslToRgb, hslToHex } from './converterFunctions';
 
 export default function App() {
 
 	const [hex, setHex] = useState(null)
 	const [rgb, setRgb] = useState(null)
 	const [hsl, setHsl] = useState(null)
+	const hexTab = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+
+	const setRandomVal = () => {
+
+		let hexColor = '#'
+
+		for (let i = 0; i < 6; i++)
+			hexColor += hexTab[Math.floor(Math.random() * 16) + 1]
+
+		document.getElementsByClassName('app')[0].style.backgroundColor = hexColor
+
+		hexInput.current.style.backgroundColor = hexColor
+		rgbInput.current.style.backgroundColor = hexColor
+		hslInput.current.style.backgroundColor = hexColor
+
+	}
+
+	const handleInput = (type) => {
+
+		switch(type) {
+
+			//case 'hex':
+
+
+		}
+		
+	}
 
 	useEffect(() => {
-		document.getElementsByClassName('app')[0].style.backgroundColor = hex
+		setRandomVal()
+		console.log(rgbToHex(255, 128, 50))
 	}, [])
+
+	const hexInput = useRef()
+	const rgbInput = useRef()
+	const hslInput = useRef()
 
     return (
       <div className="app">
@@ -18,15 +52,15 @@ export default function App() {
 			<Form>
 				<Form.Group>
 					<Form.Label>HEX</Form.Label>
-					<Form.Control type="text" placeholder="#FF8032" value="#FF8032" />
+					<Form.Control type="text" placeholder="#FF8032" value="#FF8032" ref={hexInput} onInput={(e) => handleInput('hex')} />
 				</Form.Group>
 				<Form.Group>
 					<Form.Label>RGB</Form.Label>
-					<Form.Control type="text" placeholder="rgb(255, 128, 50)" value="rgb(255, 128, 50)" />
+					<Form.Control type="text" placeholder="rgb(255, 128, 50)" value="rgb(255, 128, 50)"ref={rgbInput} />
 				</Form.Group>
 				<Form.Group>
 					<Form.Label>HSL</Form.Label>
-					<Form.Control type="text" placeholder="hsl(22.8, 100, 59.8)" value="hsl(22.8, 100, 59.8)" />
+					<Form.Control type="text" placeholder="hsl(22.8, 100, 59.8)" value="hsl(22.8, 100, 59.8)"ref={hslInput} />
 				</Form.Group>
 			</Form>
 		</div>
---
+++ src/converterFunctions.js
@@ -0,0 +1,137 @@
+const hexTab = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+
+export function rgbToHex(r, g, b) {
+
+    if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
+        return ("Invalid value(s)")
+    }
+    
+    let hexRed = hexTab[Math.floor(r / 16)]+hexTab[r % 16]
+
+    let hexGreen = hexTab[Math.floor(g / 16)]+hexTab[g % 16]
+
+    let hexBlue = hexTab[Math.floor(b / 16)] + hexTab[b % 16]
+
+    return [hexRed, hexGreen, hexBlue]
+
+}
+
+export function rgbToHsl(r, g, b) {
+
+    if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255) {
+        return ("Invalid value(s)")
+    }
+
+    let red = r / 255
+    let green = g / 255
+    let blue = b / 255
+    let max_val = Math.max(red, green, blue)
+    let min_val = Math.min(red, green, blue)
+    let d = max_val - min_val
+
+    let lightness = (max_val + min_val) / 2
+
+    let saturation = lightness != 0 ? d / (1 - Math.abs(2 * lightness - 1)) : 0
+
+    let hue = 0
+    let segment = 0
+    let shift = 0
+
+    if (d == 0) {
+        hue = 0
+    }
+    else {
+        switch(max_val) {
+            case red:
+                segment = (green - blue) / d
+                shift = 0 / 60
+                if (segment < 0)
+                    shift = 360 / 60
+                hue = segment + shift
+                break
+            case green:
+                segment = (blue - red) / d
+                shift = 120 / 60
+                hue = segment + shift
+                break
+            case blue:
+                segment = (red - green) / d
+                shift = 240 / 60
+                hue = segment + shift
+        }
+    }
+
+    hue = (hue * 60, 1).toFixed(1)
+
+    saturation = (saturation * 100, 1).toFixed(1)
+    lightness = (lightness * 100, 1).toFixed(1)
+
+    return [hue.toString(), saturation.toString(), lightness.toString()]
+
+}
+
+export function hexToRgb(r, g, b) {
+
+    let red = r.toUpperCase().split('')
+    let rgbRed = hexTab.indexOf(red[0]) * 16 + hexTab.indexOf(red[1])
+    let green = g.toUpperCase().split('')
+    let rgbGreen = hexTab.indexOf(green[0]) * 16 + hexTab.indexOf(green[1])
+    let blue = b.toUpperCase().split('')
+    let rgbBlue = hexTab.indexOf(blue[0]) * 16 + hexTab.indexOf(blue[1])
+
+    return [rgbRed, rgbGreen, rgbBlue]
+
+}
+
+export function hexToHsl(r, g, b) {
+
+    let rgb_vals = hexToRgb(r, g, b)
+
+    return rgbToHsl(rgb_vals[0], rgb_vals[1], rgb_vals[2])
+
+}
+
+export function hslToRgb(h, s, l) {
+
+    let d = (s / 100) * (1 - Math.abs(2 * (l / 100) - 1))
+
+    let x = d * (1 - Math.abs((h / 60) % 2 - 1))
+
+    let m = (l / 100) - d / 2
+
+    let r = 0, g = 0, b = 0
+
+    if (0 <= h && h < 60)
+        r = d; g = x; b = 0
+        // r, g, b = d, x, 0
+    if (60 <= h && h < 120)
+        r = x; g = d; b = 0
+        // r, g, b = x, d, 0
+    if (120 <= h && h < 180)
+        r = 0; g = d; b = x
+        // r, g, b = 0, d, x
+    if (180 <= h && h < 240)
+        r = 0; g = x; b = d
+        // r, g, b = 0, x, d
+    if (240 <= h && h < 300)
+        r = x; g = 0; b = d
+        // r, g, b = x, 0, d
+    if (300 <= h && h < 360)
+        r = d; g = 0; b = x
+        // r, g, b = d, 0, x
+
+    let red = (r + m) * 255
+    let green = (g + m) * 255
+    let blue = (b + m) * 255
+
+    return [Math.round(red), Math.round(green), Math.round(blue)]
+
+}
+
+export function hslToHex(h, s, l) {
+
+    let rgb_vals = hslToRgb(h, s, l)
+
+    return rgbToHex(rgb_vals[0], rgb_vals[1], rgb_vals[2])
+
+}
